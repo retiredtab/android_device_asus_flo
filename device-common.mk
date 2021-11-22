65642af2ce87f593224840576192e7a8a0f9463d
followmsi <followmsi@gmail.com>
2021-11-22 16:35:18 +0100

    flo: Uprev clearkey DRM HIDL to 1.4

--- device-common.mk
+++ device-common.mk
@@ -87,7 +87,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.3-service-lazy.clearkey
+    android.hardware.drm@1.4-service-lazy.clearkey
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
