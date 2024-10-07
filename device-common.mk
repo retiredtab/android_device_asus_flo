cb17abd57c067dc6e0e5b30a516b5ae8864670a5
Bruno Martins <bgcngm@gmail.com>
2024-10-07 15:51:29 -0600

    flo: Migrate to AIDL ClearKey DRM HAL

--- device-common.mk
+++ device-common.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
