c0a0c992207e6b8c727b5426a0e7f024d58142d4
Bruno Martins <bgcngm@gmail.com>
2024-10-07 15:55:10 -0600

    flo: Migrate to Health 2.1 HAL Mandatory as of QPR3 due to compiler complaining about health 2.0 not valid.

--- device-common.mk
+++ device-common.mk
@@ -231,8 +231,8 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # Ramdisk
 PRODUCT_PACKAGES += \
