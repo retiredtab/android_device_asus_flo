9b4ff1da643118abef08966e2aed070f058be3e8
0xCAFEBABE <me.cafebabe@gmail.com>
2024-10-07 15:56:25 -0600

    flo: Switch to USB 1.3-basic HAL

--- device-common.mk
+++ device-common.mk
@@ -272,7 +272,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 # VNDK
 # Patch/hexedit DRM to look for older version of libprotobuf-cpp-lite.so
